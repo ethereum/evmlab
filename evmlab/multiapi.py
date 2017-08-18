639e1231d46a679b97c5a0763507f5a41e16126d
Martin Holst Swende <martin@swende.se>
2017-08-18 16:39:22 +0200

    Added caching to the API layer

--- evmlab/multiapi.py
+++ evmlab/multiapi.py
@@ -1,3 +1,6 @@
+
+import shelve
+
 class MultiApi(object):
 
     """ Helper class for using several API:s. 
@@ -11,19 +14,50 @@ class MultiApi(object):
         self.web3 = web3
         self.etherchain = etherchain
 
+    def _getCached(self,key):
+        db = shelve.open(".api_cache")
+        obj = None
+        if key in db:
+            obj = db[key]
+        db.close()
+        return obj
+
+    def _putCached(self,key, obj):
+        db = shelve.open(".api_cache")
+        db[key] = obj
+        db.close()
 
     def getAccountInfo(self, address, blnum = None):
         acc = {}
+
+        if blnum is not None: 
+            cachekey = "%s-%d" % (address, blnum)
+            cached = self._getCached(cachekey)
+            if cached is not None:
+                return cached
+
         if self.web3 is not None: 
             acc['balance'] = self.web3.eth.getBalance(address, blnum)
             acc['code']    = self.web3.eth.getCode(address, blnum)
             acc['nonce']   = self.web3.eth.getTransactionCount(address, blnum)
             acc['address'] = address
+
+            # cache it, but only if it's at a specific block number
+            if blnum is not None:
+                self._putCached(cachekey, acc)
+
         elif self.etherchain is not None: 
             acc = self.etherchain.getAccount(address)
+
         return acc
 
     def getTransaction(self,h):
+
+        cachekey = "tx-%s" % h
+
+        o = self._getCached(cachekey)
+        if o is not None:
+            return o
 
         translations = [("sender", "from"),
                         ("recipient", "to"),
@@ -43,12 +77,22 @@ class MultiApi(object):
             for (a,b) in translations:
                 obj_dict[b] = obj_dict[a]
 
+        self._putCached( cachekey, obj_dict)
         return obj_dict
 
     def getStorageSlot(self, addr, key, blnum = None):
+
+        if blnum is not None: 
+            cachekey = "%s-%d-%s" % (addr,blnum,key)
+            cached = self._getCached(cachekey)
+            if cached is not None:
+                return cached
+
         if self.web3:
             try:
-              return self.web3.eth.getStorageAt(addr, key, blnum)
+                value = self.web3.eth.getStorageAt(addr, key, blnum)
+                self._putCached(cachekey, value)
+                return value
             except Exception as e:
                     print(e)
                     return ""
