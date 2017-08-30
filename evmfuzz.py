9b38900fe97f7f53bef182cf96f3534124b0d1a9
Martin Holst Swende <martin@swende.se>
2017-08-30 14:00:09 +0200

    Minor

--- evmfuzz.py
+++ evmfuzz.py
@@ -34,7 +34,7 @@ def execute(code, gas = 0xFFFF, verbose = False):
 
     intrinsic_geth_gas = VMUtils.getIntrinsicGas(code)
     print("Intrinsic gas: %s" % str(intrinsic_geth_gas) )
-    sys.exit(0)
+#    sys.exit(0)
     g_out = gvm.execute(code = code, gas = gas,json=True, genesis = Genesis().export_geth())
     p_out = pvm.execute(code = code, gas = gas,json=True, genesis = Genesis().export_parity())
 
