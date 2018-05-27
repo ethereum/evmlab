c131f3555e087d72fd75c762560856bac4563f97
tintinweb <tintinweb@oststrom.com>
2018-05-27 23:19:35 +0200

    extend setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,9 @@ setup(name='Evmlab',
       description='Ethereum EVM utilities',
       author='Martin Holst Swende',
       author_email='martin.swende@ethereum.org',
+      keywords=["ethereum", "transaction", "debugger"],
+      url="https://github.com/holiman/evmlab/",
+      #download_url="https://github.com/holiman/evmlab/tarball/<tag>",
       packages=['evmlab'],
-     )
+      install_requires=["requests", "web3", "urwid", "eth-hash[pycryptodome]", "rlp>=1.0", "docker==3.0.0"],
+      )
