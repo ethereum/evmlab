224a2b3c0e91798939745ad7cb72ad75cae44f5f
tintinweb <tintinweb@oststrom.com>
2018-06-12 19:56:10 +0200

    split up dependencies in hard requirements and optional/extra dependencies. see readme for how to install extras

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@
 
 from distutils.core import setup
 
+version = '0.1'
+
 setup(name='Evmlab',
-      version='0.1',
+      version=version,
       description='Ethereum EVM utilities',
       author='Martin Holst Swende',
       author_email='martin.swende@ethereum.org',
       keywords=["ethereum", "transaction", "debugger"],
       url="https://github.com/holiman/evmlab/",
-      #download_url="https://github.com/holiman/evmlab/tarball/<tag>",
+      download_url="https://github.com/holiman/evmlab/tarball/v%s"%version,
       packages=['evmlab'],
-      install_requires=["requests", "web3", "urwid", "eth-hash[pycryptodome]", "rlp>=1.0", "docker==3.0.0"],
+      install_requires=["requests", "web3", "eth-hash[pycryptodome]", "rlp>=1.0"],
+      extras_require={"consolegui": ["urwid"],
+                      "abidecoder": ["ethereum-input-decoder"],
+                      "docker": ["docker==3.0.0"]}
       )
